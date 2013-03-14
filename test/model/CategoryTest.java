c1145492ccfc4bbffbfdbabbbc5b627633008008
igor <igor.veremchuk@gmail.com>
2013-03-14 10:09:11 +0200

    added test directory

---
+++ test/model/CategoryTest.java
@@ -0,0 +1,29 @@
+package model;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: igor
+ * Date: 13.03.13
+ * Time: 21:31
+ * To change this template use File | Settings | File Templates.
+ */
+public class CategoryTest extends TestCase {
+    @Test
+    public void testGetName() throws Exception {
+
+    }
+
+    @Test
+    public void testSetName() throws Exception {
+
+    }
+
+    @Test
+    public void testToString() throws Exception {
+
+
+    }
+}
